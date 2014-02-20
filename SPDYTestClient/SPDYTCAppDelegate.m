7a8d4fbc796c0519001049b0a37842971ff1d4c2
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-02-20 12:08:09 +0100

    added empty XCode project

---
+++ SPDYTestClient/SPDYTCAppDelegate.m
@@ -0,0 +1,12 @@
+#import "SPDYTCAppDelegate.h"
+
+@implementation SPDYTCAppDelegate
+
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
---
+++ SPDYTestClientTests/SPDYTestClientTests.m
@@ -0,0 +1,17 @@
+#import <XCTest/XCTest.h>
+
+@interface SPDYTestClientTests : XCTestCase
+
+@end
+
+@implementation SPDYTestClientTests
+
+- (void)setUp {
+	[super setUp];
+}
+
+- (void)tearDown {
+	[super tearDown];
+}
+
+@end
